3370014c40e0d764def10f19358e10bb25bc08bd
Wayne Piekarski <waynepie@google.com>
2015-06-15 16:43:14 -0700

    Use simplified and common layout

--- mobile/src/main/java/net/waynepiekarski/displaylockwear/MainActivity.java
+++ mobile/src/main/java/net/waynepiekarski/displaylockwear/MainActivity.java
@@ -4,13 +4,18 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView mTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mTextView = (TextView)findViewById(R.id.lockState);
     }
 
     @Override
