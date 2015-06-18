978353985b2741fefa9349290a3612e4735f8928
Wayne Piekarski <waynepie@google.com>
2015-06-18 14:59:59 -0700

    Use play-services-wearable and remove AppCompat in order to save APK size
    Removing play-services went from 4.7mb to 3.3mb
    Removing AppCompat went from 3.3mb to 2.7mb

--- mobile/src/main/java/net/waynepiekarski/displaylockwear/BaseActivity.java
+++ mobile/src/main/java/net/waynepiekarski/displaylockwear/BaseActivity.java
@@ -1,30 +1,7 @@
 package net.waynepiekarski.displaylockwear;
 
-import android.support.v7.app.AppCompatActivity;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.app.Activity;
 
-public class BaseActivity extends AppCompatActivity {
+public class BaseActivity extends Activity {
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
